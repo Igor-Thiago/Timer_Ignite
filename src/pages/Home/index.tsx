9d547e36d7f9ce97c822b105ba57466a5e5c2ed8
Alladin-51 <igortls20@gmail.com>
2024-10-08 19:26:44 -0300

    Primeira parte da componentização da pasta home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from 'zod'
 import { useEffect, useState } from "react";
 import { differenceInSeconds } from "date-fns";
+import { NewCycleForm } from "./NewCycleForm";
+import { CountDown } from "./Countdown";
 
 
 const newCycleFormValidationSchema = zod.object({     // Validação do campo
@@ -139,46 +141,10 @@ export function Home(){
     return (
         <HomeContainer>
             <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
-                <FormContainer>
-                    <label htmlFor="task">Vou trabalhar em</label>
-                    <TaskInput 
-                        id="task" 
-                        placeholder="Dê um nome para o seu projeto"
-                        list="task-suggestions"
-                        disabled={!!activeCycle}
-                        {...register('task')}
-    
-                    />
+                <NewCycleForm/>
+                <CountDown/>
 
-                    <datalist id="task-suggestions">
-                        <option value="Projeto 1"/>
-                        <option value="Projeto 2"/>
-                        <option value="Projeto 3"/>
-                        <option value="Projeto 4"/>
-                    </datalist>
-
-                    <label htmlFor="minutesAmount">durante</label>
-                    <MinutesAmountInput 
-                        type="number" 
-                        id="minutesAmount"
-                        placeholder="00"
-                        step={5}
-                        min={5}
-                        max={60}
-                        disabled={!!activeCycle}
-                        {...register('minutesAmount', {valueAsNumber: true})}
-                    />
-
-                    <span>minutos.</span>
-                </FormContainer>
-
-                <CountDownContainer>
-                    <span>{minutes[0]}</span>
-                    <span>{minutes[1]}</span>
-                    <Separator>:</Separator>
-                    <span>{seconds[0]}</span>
-                    <span>{seconds[1]}</span>
-                </CountDownContainer>
+                
 
                 { activeCycle ? (
                     <StopCountDownButton onClick={handleInterruptCycle}  type="button">
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -17,50 +17,6 @@ export const HomeContainer = styled.main`
 
 
 `
-
-export const FormContainer = styled.div`
-
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 0.5rem;
-    color: ${(props) => props.theme['gray-100']};
-    font-size: 1,125rem;
-    font-weight: bold;
-    flex-wrap: wrap;
-
-
-`
-
-export const CountDownContainer = styled.div`
-    font-family: 'Roboto Mono', monospace;
-    font-size: 10rem;
-    line-height: 8rem;
-    color: ${(props) => props.theme['gray-100']};
-
-    display: flex;
-    gap: 1rem;
-
-    span {
-        background: ${(props) => props.theme['gray-700']};
-        padding: 2rem 1rem;
-        border-radius: 8px;
-    }
-
-`
-export const Separator = styled.div`
-
-    padding: 2rem 0;
-    color: ${(props) => props.theme['green-500']};
-
-    width: 4rem;
-    overflow: hidden;
-    display: flex;
-    justify-content: center;
-
-`
-
 export const BaseCountDownButton = styled.button`
     width: 100%;
     border: 0;
@@ -86,41 +42,6 @@ export const BaseCountDownButton = styled.button`
     
 
 `
-const BaseInput = styled.input`    // Constante criada para ser usada como base para os inputs
-    background: transparent;
-    height: 2.5rem;
-    border: 0;
-    border-bottom: 2px solid ${(props) => props.theme['gray-500']};;
-    font-weight: bold;
-    font-size: 1,125 rem;
-    padding: 0 0.5rem;
-    color: ${(props) => props.theme['gray-100']};;
-
-    &:focus{
-        box-shadow: none;
-        border-color: ${(props) => props.theme['green-500']};
-    }
-
-    &::placeholder{
-        color: ${(props) => props.theme['gray-500']};;
-    }
-`
-
-export const TaskInput = styled(BaseInput)`  // Ao invés de .input colocamos assim para ele herdar o css da constante criada
-    flex: 1;
-
-    &::-webkit-calendar-picker-indicator {
-        display: none !important;
-    }
-
-`
-
-export const MinutesAmountInput = styled(BaseInput)`
-
-    width: 4rem;
-
-`
-
 export const StartCountDownButton = styled(BaseCountDownButton)`
 
 background: ${(props) => props.theme['green-500']};
