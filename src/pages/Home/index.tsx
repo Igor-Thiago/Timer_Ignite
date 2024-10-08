1b6984e8f3f26ed7079680d207080cdd79fff6c3
Alladin-51 <igortls20@gmail.com>
2024-10-08 16:40:13 -0300

    Adicionando primeira parte da funcionalidade do timer decrescendo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,8 @@ import { CountDownContainer, FormContainer, HomeContainer, MinutesAmountInput, S
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from 'zod'
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { differenceInSeconds } from "date-fns";
 
 
 const newCycleFormValidationSchema = zod.object({     // Validação do campo
@@ -20,13 +21,16 @@ interface Cycle {
     id: string
     task: string
     minutesAmount: number
+    startDate: Date
 }
 
 export function Home(){
 
     const [cycles, setCycles] = useState<Cycle[]>([])
     const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
-    
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
+
     const {register, handleSubmit, watch, reset} = useForm<NewCycleFormData>({  // register -> retorna todas as funções de um input || watch -> permite assistir(acompanhar) algo
         resolver: zodResolver(newCycleFormValidationSchema),
         defaultValues: {
@@ -37,7 +41,16 @@ export function Home(){
         }
     })  
 
+    const activeCycle = cycles.find((cycle) => cycle.id == activeCycleId)
 
+    useEffect(() => {
+        if(activeCycle) {
+            setInterval(() => {
+                setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate)
+                )
+            }, 1000);
+        }
+    }, [activeCycle])
 
     function handleCreateNewCycle(data: NewCycleFormData){
         const id = String(new Date().getTime())
@@ -46,6 +59,7 @@ export function Home(){
             id,
             task: data.task,
             minutesAmount: data.minutesAmount,
+            startDate: new Date(),
         }
         setCycles((state) => [...state, newCycle])
         setActiveCycleId(id)
@@ -53,7 +67,19 @@ export function Home(){
 
     }
 
-    const activeCycle = cycles.find((cycle) => cycle.id == activeCycleId)
+    
+
+    const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+
+    const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+    const minutesAmount = Math.floor(currentSeconds / 60)  // Math.floor arredonda pra baixo o resultado
+
+    const secondsAmount = currentSeconds % 60
+
+    const minutes = String(minutesAmount).padStart(2, '0')
+
+    const seconds = String(secondsAmount).padStart(2, '0')
 
     const task = watch('task')  // Com isso consigo saber se o input vai ou não estar vazio || transforma o componente em um controled || monitoramento
     return (
@@ -91,11 +117,11 @@ export function Home(){
                 </FormContainer>
 
                 <CountDownContainer>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{minutes[0]}</span>
+                    <span>{minutes[1]}</span>
                     <Separator>:</Separator>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{seconds[0]}</span>
+                    <span>{seconds[1]}</span>
                 </CountDownContainer>
 
                 <StartCountDownButton disabled={!task} type="submit">
